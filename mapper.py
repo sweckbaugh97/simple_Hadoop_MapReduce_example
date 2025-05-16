9f799c34a165e24039101f87b25651c28757499c
Sean Weckbaugh <weckb694@regis.edu>
2025-05-16 17:41:19 +0000

    added more stopwords

--- mapper.py
+++ mapper.py
@@ -6,7 +6,8 @@ for line in sys.stdin:
     line = line.strip()
     # split the line into words; splits on any whitespace
     words = line.split()
-    stopwords = ('the', 'and', '.', '!', ',')
+    stopwords = ('the', 'and', 'to', 'of', 'you', 'a', 'an', 'I', '.', '!')
+    stopwords = stopwords + (',', ';', 'my', ':', '?', 'in', 'is', 'not', 'that', 'me', 'And', 'it')
     # output tuples (word, 1) in tab-delimited format
     for word in words:
         if word not in stopwords:
