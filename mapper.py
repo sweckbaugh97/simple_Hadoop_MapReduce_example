355db843a81ab872d2471c142105c6a7f5e32a06
Sean Weckbaugh <weckb694@regis.edu>
2025-05-17 19:27:06 +0000

    added NLTK stop words

--- mapper.py
+++ mapper.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
 import sys
-# get all lines from stdin
+#import nltk
+#nltk.download('stopwords')
+#from nltk.corpus import stopwords
+
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words; splits on any whitespace
     words = line.split()
-    stopwords = ('the', 'and', 'to', 'of', 'you', 'a', 'an', 'I', '.', '!')
-    stopwords = stopwords + (',', ';', 'my', ':', '?', 'in', 'is', 'not', 'that', 'me', 'And', 'it')
+    #stopwords = ('the', 'and', 'to', 'of', 'you', 'a', 'an', 'I', '.', '!')
+    #stopwords = stopwords + (',', ';', 'my', ':', '?', 'in', 'is', 'not', 'that', 'me', 'And', 'it')
     # output tuples (word, 1) in tab-delimited format
+    #stopwords_array = stopwords.words('english')
+    file_path = 'nltk_stop_words.txt'
+    stopwords = ['.', ',', '!', '?', ':', ';']
+    with open(file_path, 'r') as file:
+        stopword_lines = file.readlines()
+        #print(stopword_lines)
+        for stopword_line in stopword_lines:
+            stopwords.append(stopword_line.strip())
+    #print(stopwords)
+    #with open(file_path, 'w') as file:
+    #    for word in stopwords_array:
+    #        file.write(word + '\n')
     for word in words:
         if word not in stopwords:
             print(word + '\t1')
